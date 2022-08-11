defef61495291526812452850b8e4f5f6d73e2e1
Christopher Mendez <mcmchris@hotmail.com>
2022-08-11 15:12:11 -0400

    Add example

--- RAK4631/RAK4631_Inference_accelerometer/src/main.cpp
+++ RAK4631/RAK4631_Inference_accelerometer/src/main.cpp
@@ -20,8 +20,13 @@
  * SOFTWARE.
  */
 
-// If your target is limited in memory remove this macro to save 10K RAM
-#define EIDSP_QUANTIZE_FILTERBANK   0
+/* Includes ---------------------------------------------------------------- */
+#include <WisBLOCK_inferencing.h>
+#include "SparkFunLIS3DH.h"
+
+/* Constant defines -------------------------------------------------------- */
+#define CONVERT_G_TO_MS2    9.80665f
+#define MAX_ACCEPTED_RANGE  2.0f        // starting 03/2022, models are generated setting range to +-2, but this example use Arudino library which set range to +-4g. If you are using an older model, ignore this value and use 4.0f instead
 
 /*
  ** NOTE: If you run into TFLite arena allocation issue.
@@ -38,82 +43,95 @@
  ** If the problem persists then there's not enough memory for this model and application.
  */
 
-/* Includes ---------------------------------------------------------------- */
-#include <PDM.h>
-#include <WisBlock_WakeUp_inferencing.h> 
-
-#define PDM_DIN 21  //D8 //21
-#define PDM_CLK 4   //A0  //4
-#define PDM_PWR -1  //D10 //-1
-
-// Forward definitions
-static bool microphone_inference_start(uint32_t n_samples);
-static bool microphone_inference_record(void);
-static int microphone_audio_signal_get_data(size_t offset, size_t length, float *out_ptr);
-static void microphone_inference_end(void);
-
-/** Audio buffers, pointers and selectors */
-typedef struct {
-    int16_t *buffer;
-    uint8_t buf_ready;
-    uint32_t buf_count;
-    uint32_t n_samples;
-} inference_t;
-
-static inference_t inference;
-static signed short sampleBuffer[2048];
+/* Private variables ------------------------------------------------------- */
 static bool debug_nn = false; // Set this to true to see e.g. features generated from the raw signal
 
+
+LIS3DH SensorTwo(I2C_MODE, 0x18);
+
 /**
- * @brief      Arduino setup function
- */
+* @brief      Arduino setup function
+*/
 void setup()
 {
     // put your setup code here, to run once:
     Serial.begin(115200);
-
     Serial.println("Edge Impulse Inferencing Demo");
 
-    // summary of inferencing settings (from model_metadata.h)
-    ei_printf("Inferencing settings:\n");
-    ei_printf("\tInterval: %.2f ms.\n", (float)EI_CLASSIFIER_INTERVAL_MS);
-    ei_printf("\tFrame size: %d\n", EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE);
-    ei_printf("\tSample length: %d ms.\n", EI_CLASSIFIER_RAW_SAMPLE_COUNT / 16);
-    ei_printf("\tNo. of classes: %d\n", sizeof(ei_classifier_inferencing_categories) / sizeof(ei_classifier_inferencing_categories[0]));
+    if (SensorTwo.begin() != 0) {
+        ei_printf("Failed to initialize IMU!\r\n");
+    }
+    else {
+        ei_printf("IMU initialized\r\n");
+    }
 
-    if (microphone_inference_start(EI_CLASSIFIER_RAW_SAMPLE_COUNT) == false) {
-        ei_printf("ERR: Failed to setup audio sampling\r\n");
+    if (EI_CLASSIFIER_RAW_SAMPLES_PER_FRAME != 3) {
+        ei_printf("ERR: EI_CLASSIFIER_RAW_SAMPLES_PER_FRAME should be equal to 3 (the 3 sensor axes)\n");
         return;
     }
 }
 
 /**
- * @brief      Arduino main function. Runs the inferencing loop.
+ * @brief Return the sign of the number
+ * 
+ * @param number 
+ * @return int 1 if positive (or 0) -1 if negative
  */
+float ei_get_sign(float number) {
+    return (number >= 0.0) ? 1.0 : -1.0;
+}
+
+/**
+* @brief      Get data and run inferencing
+*
+* @param[in]  debug  Get debug info if true
+*/
 void loop()
 {
-    ei_printf("Starting inferencing in 2 seconds...\n");
+    ei_printf("\nStarting inferencing in 2 seconds...\n");
 
     delay(2000);
 
-    ei_printf("Recording...\n");
+    ei_printf("Sampling...\n");
 
-    bool m = microphone_inference_record();
-    if (!m) {
-        ei_printf("ERR: Failed to record audio...\n");
+    // Allocate a buffer here for the values we'll read from the IMU
+    float buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE] = { 0 };
+
+    for (size_t ix = 0; ix < EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE; ix += 3) {
+        // Determine the next tick (and then sleep later)
+        uint64_t next_tick = micros() + (EI_CLASSIFIER_INTERVAL_MS * 1000);
+
+        buffer[ix] = SensorTwo.readFloatAccelX();
+        buffer[ix + 1] = SensorTwo.readFloatAccelY();
+        buffer[ix + 2] = SensorTwo.readFloatAccelZ();
+
+        for (int i = 0; i < 3; i++) {
+            if (fabs(buffer[ix + i]) > MAX_ACCEPTED_RANGE) {
+                buffer[ix + i] = ei_get_sign(buffer[ix + i]) * MAX_ACCEPTED_RANGE;
+            }
+        }
+
+        buffer[ix + 0] *= CONVERT_G_TO_MS2;
+        buffer[ix + 1] *= CONVERT_G_TO_MS2;
+        buffer[ix + 2] *= CONVERT_G_TO_MS2;
+
+        delayMicroseconds(next_tick - micros());
+    }
+
+    // Turn the raw buffer in a signal which we can the classify
+    signal_t signal;
+    int err = numpy::signal_from_buffer(buffer, EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE, &signal);
+    if (err != 0) {
+        ei_printf("Failed to create signal from buffer (%d)\n", err);
         return;
     }
 
-    ei_printf("Recording done\n");
-
-    signal_t signal;
-    signal.total_length = EI_CLASSIFIER_RAW_SAMPLE_COUNT;
-    signal.get_data = &microphone_audio_signal_get_data;
+    // Run the classifier
     ei_impulse_result_t result = { 0 };
 
-    EI_IMPULSE_ERROR r = run_classifier(&signal, &result, debug_nn);
-    if (r != EI_IMPULSE_OK) {
-        ei_printf("ERR: Failed to run classifier (%d)\n", r);
+    err = run_classifier(&signal, &result, debug_nn);
+    if (err != EI_IMPULSE_OK) {
+        ei_printf("ERR: Failed to run classifier (%d)\n", err);
         return;
     }
 
@@ -129,109 +147,3 @@ void loop()
     ei_printf("    anomaly score: %.3f\n", result.anomaly);
 #endif
 }
-
-/**
- * @brief      PDM buffer full callback
- *             Get data and call audio thread callback
- */
-static void pdm_data_ready_inference_callback(void)
-{
-    int bytesAvailable = PDM.available();
-
-    // read into the sample buffer
-    int bytesRead = PDM.read((char *)&sampleBuffer[0], bytesAvailable);
-
-    if (inference.buf_ready == 0) {
-        for(int i = 0; i < bytesRead>>1; i++) {
-            inference.buffer[inference.buf_count++] = sampleBuffer[i];
-
-            if(inference.buf_count >= inference.n_samples) {
-                inference.buf_count = 0;
-                inference.buf_ready = 1;
-                break;
-            }
-        }
-    }
-}
-
-/**
- * @brief      Init inferencing struct and setup/start PDM
- *
- * @param[in]  n_samples  The n samples
- *
- * @return     { description_of_the_return_value }
- */
-static bool microphone_inference_start(uint32_t n_samples)
-{
-    inference.buffer = (int16_t *)malloc(n_samples * sizeof(int16_t));
-
-    if(inference.buffer == NULL) {
-        return false;
-    }
-
-    inference.buf_count  = 0;
-    inference.n_samples  = n_samples;
-    inference.buf_ready  = 0;
-
-    // configure the data receive callback
-    PDM.onReceive(&pdm_data_ready_inference_callback);
-
-    PDM.setPins(PDM_DIN, PDM_CLK, PDM_PWR);
-
-    PDM.setBufferSize(4096);
-
-    // initialize PDM with:
-    // - one channel (mono mode)
-    // - a 16 kHz sample rate
-    if (!PDM.begin(1, EI_CLASSIFIER_FREQUENCY)) {
-        ei_printf("Failed to start PDM!");
-        microphone_inference_end();
-
-        return false;
-    }
-
-    // set the gain, defaults to 20
-    PDM.setGain(127);
-
-    return true;
-}
-
-/**
- * @brief      Wait on new data
- *
- * @return     True when finished
- */
-static bool microphone_inference_record(void)
-{
-    inference.buf_ready = 0;
-    inference.buf_count = 0;
-
-    while(inference.buf_ready == 0) {
-        delay(10);
-    }
-
-    return true;
-}
-
-/**
- * Get raw audio signal data
- */
-static int microphone_audio_signal_get_data(size_t offset, size_t length, float *out_ptr)
-{
-    numpy::int16_to_float(&inference.buffer[offset], out_ptr, length);
-
-    return 0;
-}
-
-/**
- * @brief      Stop PDM and release buffers
- */
-static void microphone_inference_end(void)
-{
-    PDM.end();
-    free(inference.buffer);
-}
-
-#if !defined(EI_CLASSIFIER_SENSOR) || EI_CLASSIFIER_SENSOR != EI_CLASSIFIER_SENSOR_MICROPHONE
-#error "Invalid model for current sensor."
-#endif
